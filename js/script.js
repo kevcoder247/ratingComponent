5344e7afb93b7dcf5e40e8c640535b22905c17dc
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-09-25 19:12:07 -0400

    wip: when rating is clicked background color changes and value of the rating is stored in a variable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('hello')
+//Rating List
+const list = document.getElementById('rating-value');
+
+//Varaible that stores what rating was picked
+let selectedRating;
+
+//Click Event listener on list
+list.addEventListener('click', (e) => {
+  
+  //check if list item was clicked and if it was change its background color to nothing when another item is clicked
+  if(e.target.tagName == 'LI'){
+    if(selectedRating){
+      const previouslySelected = list.querySelector(`[data-value="${selectedRating}"]`)
+      if(previouslySelected){
+        previouslySelected.style.backgroundColor = '';
+      }
+    }
+    
+    //Get Value from the data attribute
+    selectedRating = e.target.getAttribute('data-value');
+    e.target.style.backgroundColor = 'grey';
+    e.target.style.color = 'white';
+
+    console.log(selectedRating)
+  }
+})
